fdb34ef7c524f300c80a69ebd8ccef3ee3a16f59
yuri danil <yuridanielsantana45@gmail.com>
2025-09-30 16:37:39 -0300

    Atualização de disponibilidade"

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ cursor = conexao.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS livros(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-titul TEXT,
+titulo TEXT,
 autor TEXT NOT NULL,
 ano INTEGER
 )            
 """)
 
-
+cursor.execute
 #inserir um  livro no banco de dados
 nome = input("digite o nome do livro que deseja cadastrar:")
 cursor.execute("""
-INSERT INTO livro (titulo,autro,ano)
+INSERT INTO livros (titulo,autor,ano)
 VALUES(?,?,?)
           
 """,)
-conexao.execute()
+
 
 def cadastrar_livro(titulo,autor,ano):
   cursor.execute()
@@ -37,3 +37,21 @@ def mostra_livro():
     for linha in cursor. fetchall():
       print(f"ID:{linha[0]} TITULO:{linha[1]} AUTOR: {linha[2]} ANO:{linha[3]} DISPONIBILIDADE:{linha[4]}")        
 conexao.close()
+
+ 
+#atualização de disponibilidade
+def atualizar_livros():
+    id = int(input("digite o id do livro que deseja pegar: "))
+    while True: 
+        disponivel = input("Deixar disponivel ou não (sim ou não): ").lower()
+        if disponivel == "sim" or disponivel == "não":
+            break
+        else:
+            print("Digite apenas sim ou não para deixar disponivel.")
+    cursor.execute("""
+    UPDATE livros
+    SET disponivel = ? 
+    WHERE id = ?
+    """, (disponivel,id)
+    )
+    
