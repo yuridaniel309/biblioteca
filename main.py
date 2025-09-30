59443829170f9c76293746c85fed714504d05487
yuri daniel <yuridanielsantana45@gmail.com>
2025-09-30 14:25:01 -0300

    listagem de livros

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ conexao = sqlite3.connect("biblioteca.db")
 cursor = conexao.cursor()
 
 cursor.execute("""
-   CREATE TABLE IF NOT EXISTS livros(
-     id INTEGER PRIMARY KEY AUTOINCREMENT"0
-      titul TEXT)
-      autor TEXT NOT NULL
-      ano INTEGER
-      disponivel CHAR(3) CHECK(disponivel IN ("SIM","Nao"))
-      )
+CREATE TABLE IF NOT EXISTS livros(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+titul TEXT,
+autor TEXT NOT NULL,
+ano INTEGER
+)            
 """)
+
 
 #inserir um  livro no banco de dados
 nome = input("digite o nome do livro que deseja cadastrar:")
@@ -23,18 +23,17 @@ VALUES(?,?,?)
 conexao.execute()
 
 def cadastrar_livro(titulo,autor,ano):
-  nome = input("digite o nome livro que deseja cadastrar:")
-  cursor.execute("""
+  cursor.execute()
+nome = input("digite o nome livro que deseja cadastrar:")
+cursor.execute("""
 INSERT INTO livros (titulo,autro,ano)
 VALUES (?,?,?)
-  )
-               
-               
-               
-               
-               
-               
-               
-               
-               
-               """)
+)
+                      
+""")          
+#mostrando os livro cadastrados
+def mostra_livro():
+    cursor.execute("SELECT * FROM livros")
+    for linha in cursor. fetchall():
+      print(f"ID:{linha[0]} TITULO:{linha[1]} AUTOR: {linha[2]} ANO:{linha[3]} DISPONIBILIDADE:{linha[4]}")        
+conexao.close()
